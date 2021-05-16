30aae6a7afa9a41aeaea78306ecadaea345ff4c4
Eduardo Cavalcante <42215395+EPEduardoPaixao@users.noreply.github.com>
2021-05-16 00:23:13 -0300

    Criação dos métodos get, post, put e delete

---
+++ Back/src/ProEventos.API/Controllers/EventController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventController : ControllerBase
+    {
+        public EventController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return(
+                 "Teste Get"
+            );
+        }
+        [HttpPost]
+        public string Post()
+        {
+            return(
+                 "Teste Post"
+            );
+        }
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return(
+                 $"Teste Put id={id}"
+            );
+        }
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return(
+                 $"Teste Delete id={id}"
+            );
+        }
+    }
+}
